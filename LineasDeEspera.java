da0afcb7fc01591fb0bc6f6cad548db74fdd6b12
IratzeReyes <andymin1591@gmail.com>
2025-12-03 23:49:09 -0600

    Termino del primer modelo de inventario M/M/!, en donde se pidieron los datos y se mostraron los resultados de acuerdo a las formulas

--- LineasDeEspera.java
+++ LineasDeEspera.java
@@ -14,10 +14,32 @@ public class LineasDeEspera {
                 //se pide primero los datos landa y Niu
                 int landa; //promedio de llegadas
                 int niu; //promedio de servicio
-                int p; //probabilidad e encontrar ocupado
+                double p; //probabilidad e encontrar ocupado
                 int lq; //numero promedio de clientes esperando en la cola 
                 int wq; //tiempo promedio de espera en la cola
                 int w; //tiempo total en el sistema
+                //pedir datos 
+                System.out.println("Introduce Landa (promedio de llegadas): ");
+                landa = lector.nextInt();
+                System.out.println("Introduce Niu (promedio de servicio): ");
+                niu = lector.nextInt();
+                //calcular formulas
+                //calcular p (utilizacion del sistema promedio)
+                p= landa/niu;
+                //calcular Lq (numero promedio de clientes en la cola)
+                lq = (int) ((Math.pow(landa, 2))/(niu*(niu-landa)));
+                //calcular Wq (tiempo promedio de espera en la cola)
+                wq = lq/landa;
+                //calcular W (tiempo total en el sistema)
+                w = wq + (1/niu);
+                //Mostrando los resultados de las formulas 
+                System.out.println("Resultados del modelo M/M/1:");
+                System.out.println("Utilizacion del sistema promedio (p): " + p); //mostrando p
+                System.out.println("Numero promedio de clientes en la cola (Lq): " + lq); //mostrando Lq
+                System.out.println("Tiempo promedio de espera en la cola (Wq): " +wq); //mostrando Wq
+                System.out.println("Tiempo total en el sistema (w): " +w); //mostrando w
+
+
 
 
                 
